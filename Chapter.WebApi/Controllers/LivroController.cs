93363299feff9441470019e4e277377a067e6fc4
Fabianna Franci <110000787+FabiannaFranci@users.noreply.github.com>
2023-01-13 15:44:50 -0300

    Crud completo

---
+++ Chapter.WebApi/Controllers/LivroController.cs
@@ -0,0 +1,78 @@
+﻿using Chapter.WebApi.Interface;
+using Chapter.WebApi.Models;
+using Chapter.WebApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livroRepository.Ler());
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(Livro livro)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(livro);
+                return Ok(livro);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+        [HttpPut]
+        public IActionResult Update(int id,Livro livro)
+        {
+            try
+            {
+                _livroRepository.Atualizar(id, livro);
+                return StatusCode(204); 
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+       
+        
+    }
+}
