09bfde4c07d648027b680a0009366778be8365e4
Fabianna Franci <110000787+FabiannaFranci@users.noreply.github.com>
2023-01-30 17:09:28 -0300

    projeto ChapterApi finalizado

---
+++ Chapter.WebApi/Controllers/LivroController.cs
@@ -0,0 +1,89 @@
+﻿using Chapter.WebApi.Interface;
+using Chapter.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly ILivroRepository _iLivroRepository;
+        public LivroController(ILivroRepository iLivroRepository)
+        {
+            _iLivroRepository = iLivroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_iLivroRepository.Ler());
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(Livro livro)
+        {
+            try
+            {
+                _iLivroRepository.Cadastrar(livro);
+                return Ok(livro);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+        [HttpPut]
+        public IActionResult Update(int id,Livro livro)
+        {
+            try
+            {
+                _iLivroRepository.Atualizar(id, livro);
+                return StatusCode(204); 
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _iLivroRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+        [HttpGet("id")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                return Ok(_iLivroRepository.BuscarPorId(id));
+            }
+            catch (Exception e )
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+       
+        
+    }
+}
---
+++ Chapter.WebApi/Repositories/UsuarioRepository.cs
@@ -0,0 +1,62 @@
+﻿using Chapter.WebApi.Contexts;
+using Chapter.WebApi.Interface;
+using Chapter.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.WebApi.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+
+        private readonly ChapterContext _chaptercontext;
+        public UsuarioRepository(ChapterContext context)
+        {
+            _chaptercontext = context;
+        }
+
+        public void Atualizar(int id, Usuario usuario)
+        {
+            Usuario usuariobuscado = _chaptercontext.Usuarios.Find(id);
+            if (usuariobuscado != null) 
+            {
+                usuariobuscado.Email = usuario.Email;
+                usuariobuscado.Senha = usuario.Senha;
+                usuariobuscado.Tipo = usuario.Tipo; 
+            }
+            _chaptercontext.Usuarios.Update(usuariobuscado);
+            _chaptercontext.SaveChanges();
+        }
+
+        public void Cadastrar(Usuario usuario)
+        {
+            _chaptercontext.Usuarios.Add(usuario);
+            _chaptercontext.SaveChanges();
+            
+
+        }
+
+        public void Deletar(int id)
+        {
+            Usuario usuario = _chaptercontext.Usuarios.Find(id);
+            _chaptercontext.Usuarios.Remove(usuario);
+            _chaptercontext.SaveChanges();
+
+        }
+
+        public Usuario GetById(int id)
+        {
+           return _chaptercontext.Usuarios.Find(id);
+
+        }
+
+        public List<Usuario> Listar()
+        {
+           return _chaptercontext.Usuarios.ToList();
+        }
+
+        public Usuario Login(string email, string senha)
+        {
+            return _chaptercontext.Usuarios.FirstOrDefault(u => u.Email == email && u.Senha == senha);
+        }
+    }
+}
